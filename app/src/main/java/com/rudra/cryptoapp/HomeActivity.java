deb37194ade35395d0fc5da03f3142e7482bc216
Swaugat Beura <70109308+SWAUGAT-BEURA@users.noreply.github.com>
2021-02-17 00:45:43 +0530

    Update HomeActivity.java

--- app/src/main/java/com/rudra/cryptoapp/HomeActivity.java
+++ app/src/main/java/com/rudra/cryptoapp/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.rudra.cryptoapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,19 +34,26 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuthhome;
     private SharedPreferences prefManager;
     private SharedPreferences.Editor editor;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        username=findViewById(R.id.username_display);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         prefManager = getApplicationContext().getSharedPreferences("LOGIN", MODE_PRIVATE);
         editor = prefManager.edit();
 
-        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
-        if (user!=null){
-            username.setText(user.getDisplayName());
+        Toolbar toolbar = findViewById(R.id.tl_main);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Orital");
         }
+
+
 
 
     }
@@ -64,5 +79,41 @@ public class HomeActivity extends AppCompatActivity {
             }
         }).show();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_logout:
+                new AlertDialog.Builder(HomeActivity.this).setTitle("Alert")
+                        .setMessage("Are you sure you want to Logout")
+                        .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                editor.putBoolean("ISLOGGEDIN", false);
+
+                                editor.apply();
+
+                                FirebaseAuth.getInstance().signOut();
+
+                                startActivity(new Intent(HomeActivity.this, Login.class));
+                                finish();
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).show();
+                break;
+        }
+        return true;
     }
 }
